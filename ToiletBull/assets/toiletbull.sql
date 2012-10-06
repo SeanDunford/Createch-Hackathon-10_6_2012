5374ec3e97e326c64bb2bdf6526f01cb1073fca9
Mark Di Sano <markdisano@gmail.com>
2012-10-06 16:03:25 -0400

    new database .sql

---
+++ ToiletBull/assets/toiletbull.sql
@@ -0,0 +1,3 @@
+BEGIN TRANSACTION;
+CREATE TABLE Bathroom (ImageLocation TEXT, UrinalDividers NUMERIC, BlowDryers NUMERIC, Cleanliness NUMERIC, GraffittiAmount NUMERIC, GraffittiQuality NUMERIC, Handicap NUMERIC, Landmark TEXT, Location TEXT, NumberOfStalls NUMERIC, NumberOfUrinals NUMERIC, OverallComfort NUMERIC, OverallRating NUMERIC, PaperTowelDispenser NUMERIC, PrivateBathroom NUMERIC, SanitaryToiletCovers NUMERIC, ToiletFlush NUMERIC, Traffic NUMERIC, WellStockedResources NUMERIC);
+COMMIT;
